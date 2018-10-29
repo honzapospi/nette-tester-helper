14d6a01304923e29d5dafad281a14f76779f0668
Jan Pospisil <me@jan-pospisil.cz>
2018-10-29 11:34:28 +0100

    PHP 7.1

--- src/PresenterHelper.php
+++ src/PresenterHelper.php
@@ -10,6 +10,7 @@ use Nette\Application\Responses\JsonResponse;
 use Nette\Application\Responses\RedirectResponse;
 use Nette\Application\Responses\TextResponse;
 use Nette\Application\UI\Presenter;
+use Nette\Application\IResponse;
 use Nette\DI\Container;
 use Tester\Assert;
 use Nette\StaticClass;
@@ -34,29 +35,29 @@ class PresenterHelper {
 	 * @param $name
 	 * @return Presenter
 	 */
-	public function createPresenter($name){
+	public function createPresenter(string $name): Presenter {
 		$presenter = $this->container->getByType('Nette\Application\IPresenterFactory')->createPresenter($name);
 		$presenter->autoCanonicalize = FALSE;
 		return $presenter;
 	}
 
-	public function processRequest(Request $request){
+	public function processRequest(Request $request): IResponse {
 		return $this->createPresenter($request->getPresenterName())->run($request);
 	}
 
-	public function textResponse($response){
+	public function textResponse($response): string {
 		Assert::type('Nette\Application\Responses\TextResponse', $response);
 		$s = (string) $response->getSource();
 		Assert::true(gettype($s) == 'string');
 		return $s;
 	}
 
-	public function jsonResponse($response){
+	public function jsonResponse($response): void{
 		Assert::type('Nette\Application\Responses\JsonResponse', $response);
 		Assert::true(is_array($response->getPayload()));
 	}
 
-	public function redirectResponse($response, $code = 302, $urlEnding = NULL){
+	public function redirectResponse($response, $code = 302, $urlEnding = NULL): void {
 		Assert::type('Nette\Application\Responses\RedirectResponse', $response);
 		Assert::true($response->getCode() == $code);
 		if($urlEnding){
